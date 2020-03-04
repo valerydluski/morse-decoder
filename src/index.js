72c02be07aa1c85f0085f4d3e133edb04315ca3a
valerydluski <valerydluski@gmail.com>
2020-03-04 11:31:58 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    for(let i=0; i<=(expr.length/10); i++){
+      arr.push(expr.slice(10*i, (i+1)*10))  
+    }
+    result='';
+    for(let j=0; j<arr.length-1; j++){
+        if(arr[j]==='**********'){
+            result += ' ';
+            continue;
+        }
+        let str = '';
+        for(let k=0; k<=arr[j].length; k +=2){
+            switch(arr[j].slice(k, k+2)){
+                case "10" : str += "."; break;
+                case "11" : str += "-"; break; 
+                default: break;  
+            }
+        }
+        result += MORSE_TABLE[str];
+    }
+    return result;
 }
+
 
 module.exports = {
     decode
